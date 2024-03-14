daeb21be61a9b7507994e85d7f6c04d5e9f7b0b9
SoongE <oh1.seungmin@gmail.com>
2024-03-14 12:26:37 +0900

    Update setup

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 apt-get -y update; apt-get -y upgrade;
 
-apt-get -y install ssh sshfs openssh-server sudo git tree zsh tmux zip rsync systemd vim wget curl tzdata autojump language-pack-en miller fd-find bat libgl1-mesa-glx ffmpeg libsm6 libxext6 iotop iftop imagemagick ranger
-# apt-get -y install build-essential ffmpeg libsm6 libxext6 libudev-dev libncurses* texlive-full
+apt-get -y install ssh sshfs openssh-server sudo git tree zsh tmux zip rsync systemd vim wget curl tzdata autojump language-pack-en miller fd-find bat iotop iftop imagemagick ranger
+# apt-get -y install build-essential ffmpeg libsm6 libxext6 libudev-dev libncurses* texlive-full libgl1-mesa-glx
 
 apt-get clean autoclean; apt-get -y autoremove --purge
 
-pip install --upgrade transformers timm wandb einops tmuxp nvitop lightning torchmetrics ujson hydra-core captum seaborn deepspeed datasets wilds accelerate
+pip install --upgrade transformers timm wandb einops tmuxp nvitop lightning torchmetrics ujson hydra-core captum seaborn deepspeed datasets wilds accelerate open_clip
 
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
