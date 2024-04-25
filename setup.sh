78b9244661b89d342e91bbf528143845ab484ddc
SoongE <oh1.seungmin@gmail.com>
2024-04-25 20:38:01 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -8,8 +8,8 @@ apt-get clean autoclean; apt-get -y autoremove --purge
 pip install --upgrade transformers timm wandb einops tmuxp nvitop lightning torchmetrics ujson hydra-core captum seaborn deepspeed datasets wilds accelerate open_clip_torch img2dataset 
 
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 chsh -s $(which zsh) $USER
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # paste skel files
 cd ~
